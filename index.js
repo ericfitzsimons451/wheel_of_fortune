9eb08055400fa51247e697aa1748300d4b1d0b27
Eric Weissman <eric.weissman21@gmail.com>
2018-12-05 15:31:30 -0700

    Set up start screen handlers

---
+++ index.js
@@ -0,0 +1,12 @@
+const player1Name = $('.player1-input').val()
+const player2Name = $(".player2-input").val();
+const player3Name = $(".player3-input").val();
+
+$('.start-game-btn').on('click', startGame)
+
+
+//Functions for Event Handlers
+function startGame() {
+  console.log('hi')
+}
+
