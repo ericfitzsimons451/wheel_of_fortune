26fe91483392e570322d020966c7f497cef7715f
Eric Weissman <eric.weissman21@gmail.com>
2018-12-05 14:23:57 -0700

    Adds boilerplate  player properties and methods

---
+++ Player.js
@@ -0,0 +1,26 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.turn = false;
+    this.roundPoints = 0;
+    this.bank = 0
+  }
+
+  spinWheel(){
+
+  }
+
+  guessLetter() {
+
+  }
+
+  solvePuzzle() {
+
+  }
+
+  buyVowel() {
+    
+  }
+}
+
+module.exports = Player;
