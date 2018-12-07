31d481ba05691c733642f85c5f3695dada2ab8bf
Eric Weissman <eric.weissman21@gmail.com>
2018-12-06 19:06:47 -0700

    Remove console logs and comments for PR

--- Game.js
+++ Game.js
@@ -12,15 +12,26 @@ class Game {
 
 
   startGame() {
-    // event.preventDefault()
     const playerNames = domUpdates.setPlayerNames();
     const test = playerNames.forEach((name) => {
       let player = new Player(name)
       this.players.push(player);
     })
     this.createPuzzleBank(data);
-    // this.players[0].turn = true;
+    this.players[0].turn = true;
     domUpdates.hideStartScreen();
+  }
+
+  createPuzzleBank(data) {
+    const puzzlesToUse = Object.keys(data.puzzles).reduce((arr, puzzleKey) => {
+      arr.push(data.puzzles[puzzleKey].puzzle_bank)
+      return arr
+    }, []).flat().sort(function () {
+      return 0.5 - Math.random()
+    }).slice(0, 5);
+    puzzlesToUse.forEach(puzzle => {
+      this.puzzles.push(puzzle)
+    })
   }
   
 
@@ -38,25 +49,6 @@ class Game {
 
   exitGame() {
     
-  }
-
-  createPuzzleBank(data) {
-    const puzzlesToUse = Object.keys(data.puzzles).reduce((arr, puzzleKey) => {
-      arr.push(data.puzzles[puzzleKey].puzzle_bank)
-      return arr
-    }, []).flat().sort(function() {
-        return 0.5 - Math.random()
-      }).slice(0, 5);
-      puzzlesToUse.forEach(puzzle => {
-        this.puzzles.push(puzzle)
-      })
-    // this.puzzles.push(puzzlesToUse)
-  }
-
-  choosePuzzle(puzzle){
-    if (this.round === 1) {
-      let puzzle = new Puzzle(this.puzzles[0])
-    }
   }
 }
 if (typeof module !== 'undefined') {
--- index.js
+++ index.js
@@ -3,32 +3,5 @@ $('.start-game-btn').on('click', function(){
   game.startGame();
 })
 
-// let puzzle;
-// //Functions for Event Handlers
-// function startGame(event) {
-//   event.preventDefault()
-
-
-//   // const player1Name = $('#player-1-input').val();
-//   // const player2Name = $("#player-2-input").val();
-//   // const player3Name = $("#player-3-input").val();
-
-//   // $('.display-player1-name').text(`Player 1: ${player1Name}`);
-//   // $('.display-player2-name').text(`Player 2: ${player2Name}`);
-//   // $('.display-player3-name').text(`Player 3: ${player3Name}`);
-//   // const playerNames = domUpdates.setPlayerNames();
-//   // const playerArr = playerNames.map((name) => {
-//   //   return new Player(name)
-//   })
-
-
- 
-  
-
-
-//   // game = new Game(playerArr);
-//   puzzle = game.createPuzzle(data);
-
-// }
 
 
