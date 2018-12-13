4ad1790dd9aba7b8c111e131fa5a3edc8c1a0047
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2018-12-13 16:14:50 -0700

    adds tests, breaks a few new ones

--- test/Player-test.js
+++ test/Player-test.js
@@ -11,45 +11,43 @@ global.Puzzle = require('../Puzzle.js')
 
 
 describe('Player', function() {
+  var game;
   var player;
+  var names = ['jerry', 'john', 'ricky']
   
   beforeEach(function() {
-    // chai.spy.on(global.domUpdates, [
-    //   'generateRandomPuzzle',
-    //   'hideStartScreen',
-    //   'displayCurrentPlayerTurn',
-    //   'displayPuzzle',
-    //   'displayCategory',
-    //   'createPlayers',
-    //   'updatePlayerRoundScore',
-    //   'fireNameAlert',
-    //   'fireVowelAlert',
-    //   'displaySpinValue',
-    //   'showGuessedLetter',
-    //   'updatePuzzleOnDom',
-    //   'putVowelOnDom',
-    //   'deductVowelCost',
-    //   'displaySolvedPuzzle',
-    //   'enableButtons',
-    //   'displayRoundPopUp',
-    //   'hideRoundPopUp',
-    //   'updateRoundNumber',
-    //   'displayTotalScore',
-    //   'resetVowels',
-    //   'clearPuzzle',
-    //   'resetRoundScore',
-    //   'forRoundChange',
-    //   'forStartingGame',
-    //   'forCorrectSolution'], () => true) 
+    chai.spy.on(global.domUpdates, [
+      'generateRandomPuzzle',
+      'hideStartScreen',
+      'displayCurrentPlayerTurn',
+      'displayPuzzle',
+      'displayCategory',
+      'createPlayers',
+      'updatePlayerRoundScore',
+      'fireNameAlert',
+      'fireVowelAlert',
+      'displaySpinValue',
+      'showGuessedLetter',
+      'updatePuzzleOnDom',
+      'putVowelOnDom',
+      'deductVowelCost',
+      'displaySolvedPuzzle',
+      'enableButtons',
+      'displayRoundPopUp',
+      'hideRoundPopUp',
+      'updateRoundNumber',
+      'displayTotalScore',
+      'resetVowels',
+      'clearPuzzle',
+      'resetRoundScore',
+      'forRoundChange',
+      'forStartingGame',
+      'forCorrectSolution', 
+      'implementsGameStart'], () => true) 
 
-    let players = [
-    player1 = new Player(),
-    player2 = new Player(),
-    player3 = new Player(),
-    ]
-    
+   
     game = new Game();
-    game.players = players;
+    game.startGame(names)
     game.currentPuzzle = new Puzzle();
 
   });
@@ -59,8 +57,9 @@ describe('Player', function() {
   })
 
   it('should have a name', function() {
-    game.players[0].name = 'Bob'
-    expect(game.players[0].name).to.equal("Bob");
+    // chai.spy.on(global.domUpdates, ['setPlayerNames', 'implementsGameStart'], () => true ) 
+    game.players[0].name = 'jerry'
+    expect(game.players[0].name).to.equal("jerry");
   });
 
 
@@ -70,13 +69,23 @@ describe('Player', function() {
   //it should change player turn adn increment score down if spin is bankrupt and call deductVowelCost
 
   it('should be able to spin that wheel', function() {
-    chai.spy.on(global.domUpdates, ['deductVowelCost', 'displaySpinValue'], () => true)
+    // chai.spy.on(global.domUpdates, ['deductVowelCost', 'displaySpinValue'], () => true)
     game.players[0].spinWheel();
     expect(domUpdates.displaySpinValue).to.have.been.called(1);
   })
 
+  it('should have round points reset if they spin a bankrupt', function() {
+    game.players[0].spinWheel("BANKRUPT");
+    game.players[0].roundPoints = 0;
+  }),
+
+  it('should change players when the wheel returns LOSE A TURN', function() {
+
+
+  })
+
   it('should be able to guess a letter', function() {
-    chai.spy.on(global.domUpdates, ['showGuessedLetter', 'updatePuzzleOnDom', 'fireVowelAlert', 'updatePlayerRoundScore', 'displayCurrentPlayerTurn'], () => true)
+    // chai.spy.on(global.domUpdates, ['showGuessedLetter', 'updatePuzzleOnDom', 'fireVowelAlert', 'updatePlayerRoundScore', 'displayCurrentPlayerTurn'], () => true)
     game.players[0].turn = true;
     game.players[1].turn = false;
     game.players[2].turn = false;
